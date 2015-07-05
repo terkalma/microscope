6f4783551e23bf9644fe634b5fa6fce5de2d62fa
Róbert Terkál <terkalma@gmail.com>
2015-07-05 21:27:02 +0200

    finishing chapter 9.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,16 @@
+Template.postSubmit.onCreated(function () {
+    Session.set('postSubmitErrors', {});
+});
+
+Template.postSubmit.helpers({
+    errorMessage: function(field) {
+        return Session.get('postSubmitErrors')[field];
+    },
+    errorClass: function (field) {
+        return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+    }
+});
+
 Template.postSubmit.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -6,11 +19,25 @@ Template.postSubmit.events({
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val()
         };
+
+        var errors = validatePost(post);
+        if (errors.title || errors.url) {
+            return Session.set('postSubmitErrors', errors);
+        }
         
         Meteor.call('postInsert', post, function(error, result) {
-            if (error)
-                return alert(error.reason);
-            Router.go('postPage', {_id: result._id});
+            if (error) {
+                return throwError(error.reason);
+            }
+
+            if (result.postExists) {
+                throwError('This link has already been posted');
+            }
+
+            // [TODO] decide where to redirect
+            //Router.go('postPage', {_id: result._id});
         });
+
+        Router.go('postsList');
     }
 });
